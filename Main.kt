7b73ba14607f1782d11cfa97a662babced6b6778
nikolaimerritt <nmerritt.62761@farnborough.ac.uk>
2017-10-13 16:15:44 +0100

    Finished UI "skeleton"

--- Main.kt
+++ Main.kt
@@ -2,17 +2,12 @@ package sample
 
 import javafx.application.Application
 import javafx.collections.FXCollections
-import javafx.embed.swt.FXCanvas
 import javafx.scene.Scene
-import javafx.scene.control.Button
 import javafx.scene.layout.GridPane
 import javafx.stage.Stage
 import javafx.geometry.Insets
 import javafx.geometry.Orientation
-import javafx.scene.control.ListView
-import javafx.scene.control.ProgressBar
-import javafx.scene.control.TextField
-import javafx.event.ActionEvent
+import javafx.scene.control.*
 import java.util.*
 
 class Main : Application()
@@ -22,7 +17,7 @@ class Main : Application()
     {
         // initialising stage
         val grid = GridPane()
-        val scene = Scene(grid, 200.0, 200.0)
+        val scene = Scene(grid)
         stage.scene = scene
         stage.title = "Music Manager"
         stage.setOnCloseRequest { System.exit(0) }
@@ -48,35 +43,54 @@ class Main : Application()
         GridPane.setConstraints(plusButton, 98, 0)
         grid.children.add(plusButton)
 
+        // add drop-down playlist menu
+        val playlistMenu = ComboBox<String>()
+        GridPane.setConstraints(playlistMenu, 0 ,1, 98, 1)
+        grid.children.add(playlistMenu)
+
         // add song table
         val listView = ListView<String>(FXCollections.observableArrayList(trackQueue))
         listView.orientation = Orientation.VERTICAL
-        GridPane.setConstraints(listView, 0, 1, 100, 1)
+        GridPane.setConstraints(listView, 0, 2, 100, 1)
         grid.children.add(listView)
 
         // add play/pause button
         val playPauseButton = Button("▮▶")
         playPauseButton.setOnAction { playPause() }
-        GridPane.setConstraints(playPauseButton, 0, 2)
+        GridPane.setConstraints(playPauseButton, 0, 3)
         grid.children.add(playPauseButton)
 
         // add progress bar
         val progressBar = ProgressBar(0.0)
         progressBar.maxWidth = Double.MAX_VALUE
-        progressBar.onMouseClicked {(event) -> seek(event)}
+        GridPane.setConstraints(progressBar, 1, 3, 97, 1)
+        grid.children.add(progressBar)
+
+        // add skip backwards button
+        val skipBackwardsButton = Button("◀")
+        skipBackwardsButton.setOnMouseClicked { seek() }
+        GridPane.setConstraints(skipBackwardsButton, 98, 3)
+        grid.children.add(skipBackwardsButton)
+
+        // add skip forwards button
+        val skipForwardsButton = Button("▶")
+        skipForwardsButton.setOnMouseClicked { seek() }
+        GridPane.setConstraints(skipForwardsButton, 99, 3)
+        grid.children.add(skipForwardsButton)
 
         stage.show()
     }
 
-    private fun login() {}
-    private fun addSong() {}
-    private fun playPause() {}
-    private fun seek(event: javafx.event.ActionEvent) {}
+    private fun login() = showNotImplemented()
+    private fun addSong() = showNotImplemented()
+    private fun playPause() = showNotImplemented()
+    private fun seek() = showNotImplemented()
+
+    private fun showNotImplemented() = Alert(Alert.AlertType.WARNING, "Not implemented yet... But watch this space!").showAndWait()
 
     companion object
     {
         @JvmStatic
         fun main(args: Array<String>) { launch(Main::class.java) }
     }
-
 }
